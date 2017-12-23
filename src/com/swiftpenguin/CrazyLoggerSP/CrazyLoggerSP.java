f52762e4f4ddfc2b98ef98e0300b708cf2f60269
Will <williameaster@hotmail.com>
2017-12-23 02:30:23 +0000

    Add files via upload

---
+++ src/com/swiftpenguin/CrazyLoggerSP/CrazyLoggerSP.java
@@ -0,0 +1,74 @@
+package com.swiftpenguin.CrazyLoggerSP;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CrazyLoggerSP extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(new Logger(this), this);
+        registerConfig();
+
+    }
+
+    private void registerConfig() {
+        saveDefaultConfig();
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("crazylogger") && sender instanceof Player) {
+            if (!sender.hasPermission("crazylogger.lookup")) {
+
+            } else {
+
+                if (args.length == 0) {
+                    sender.sendMessage(ChatColor.RED + "Please give a IGN");
+                    return true;
+
+                } else if (args[0] != null) {
+
+                    if (!sender.hasPermission("crazylogger.lookup")) {
+
+                    } else {
+                        String p = args[0];
+
+                        if (getConfig().getInt("Data." + p + "." + "total") == 0) {
+                            sender.sendMessage(ChatColor.RED + "Could not find that person in the data... Names are cap sensitive...");
+                            return true;
+
+                        } else {
+
+                            sender.sendMessage(ChatColor.BLUE +p+"'s"+ " Data printed out in console...");
+
+                            ConfigurationSection configSection = getConfig().getConfigurationSection("Data." + p + "." + "info");
+                            for (String key : configSection.getKeys(false)) {
+                                System.out.println(configSection.getString(key));
+
+                            }
+
+                            int total = getConfig().getInt("Data." + p + "." + "total");
+                            System.out.println(ChatColor.RED + p + "'s" + " Total " + total);
+
+                        }
+                    }
+
+                    return true;
+                }
+
+
+            }
+
+        }
+        return true;
+    }
+    }
---
+++ src/com/swiftpenguin/CrazyLoggerSP/Logger.java
@@ -0,0 +1,51 @@
+package com.swiftpenguin.CrazyLoggerSP;
+
+import me.badbones69.crazycrates.api.events.PlayerPrizeEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Logger implements Listener {
+
+    private CrazyLoggerSP plugin;
+
+    public Logger(CrazyLoggerSP plugin) {
+        this.plugin = plugin;
+
+    }
+
+    @EventHandler
+    public void prizeLogger(PlayerPrizeEvent e) {
+
+        String p = e.getPlayer().getName();
+        String crate = e.getCrateName();
+        String prize = e.getPrize().getDisplayItem().getItemMeta().getDisplayName();
+
+        if (!plugin.getConfig().getBoolean("ignore-ops")){
+
+                DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                Date date = new Date();
+                int counter = plugin.getConfig().getInt("Data." + p + ".total") + 1;
+
+                plugin.getConfig().set("Data." + p + ".total", counter);
+                plugin.getConfig().set("Data." + p + ".info." + counter, dateFormat.format(date) + " " + p + " Crate: " + crate + " Reward: " + prize);
+                plugin.saveConfig();
+
+            } else  if (e.getPlayer().isOp() || e.getPlayer().hasPermission("crazylogger.exempt")) {
+                return;
+
+            }
+                DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                Date date = new Date();
+                int counter = plugin.getConfig().getInt("Data." + p + ".total") + 1;
+
+                plugin.getConfig().set("Data." + p + ".total", counter);
+                plugin.getConfig().set("Data." + p + ".info." + counter, dateFormat.format(date) + " " + p + " Crate: " + crate + " Reward: " + prize);
+                plugin.saveConfig();
+
+        }
+}
+
